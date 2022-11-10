4721312093591dc162dc09b8c2f1c7c321ecfa64
JDubas94 <uliadubas94@gmail.com>
2022-11-10 13:50:06 +0200

    Add className "card"

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ function App() {
     <div className="wrapper">
       <header>
         <div className="headerLeft">
-          <img width={40} height={40} src="/image/Dress.png" />
+          <img width={40} height={40} src="/image/Dress.png" alt="Dress" />
           <div>
             <h3>React Dress</h3>
             <p>Stylish dress shop</p>
@@ -11,16 +11,30 @@ function App() {
         </div>
         <ul className="headerRight">
           <li>
-            <img width={18} height={18} src="/image/Basket.svg" />
+            <img width={18} height={18} src="/image/Basket.svg" alt="Basket" />
             <span>100$</span>
           </li>
           <li>
-            <img width={18} height={18} src="/image/user.svg" />
+            <img width={18} height={18} src="/image/user.svg" alt="user" />
           </li>
         </ul>
       </header>
       <div className="content">
         <h1>All Dress</h1>
+
+        <div className="card">
+          <img width={250} height={333} src="image/Dress/1.png" alt="Dress" />
+          <p>Women's dress</p>
+          <div>
+            <div>
+              <span>Price: </span>
+              <b>100$</b>
+            </div>
+            <button>
+              <img width={11} height={11} src="image/plus.svg" alt="Plus" />
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
